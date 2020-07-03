4f0a6218a3e79d7bb571a9094073c0db6d397b7a
linwei <61023573+yutushe@users.noreply.github.com>
2020-07-03 14:21:36 +0800

    **

--- Script/txnews.js
+++ Script/txnews.js
@@ -42,13 +42,13 @@ hostname = api.inews.qq.com
 Cookie获取后，请注释掉Cookie地址。
 
 */
-const notifyInterval = 5 //视频红包间隔通知开为1，常关为0
-const logs = 1; // 日志开关，0为关，1为开
+const notifyInterval = 3 //视频红包间隔通知开为1，常关为0
+const logs = 0; // 日志开关，0为关，1为开
 const cookieName = '腾讯新闻'
 const sy = init()
-const signurlVal = sy.getdata('sy_signurl_txnews2')
-const cookieVal = sy.getdata( 'sy_cookie_txnews2')
-const videoVal = sy.getdata( 'video_txnews2')
+const signurlVal = sy.getdata('sy_signurl_txnews')
+const cookieVal = sy.getdata( 'sy_cookie_txnews')
+const videoVal = sy.getdata( 'video_txnews')
 
 let isGetCookie = typeof $request !== 'undefined'
 if (isGetCookie) {
@@ -63,14 +63,14 @@ if ($request && $request.method != 'OPTIONS' && $request.url.match(/user\/event\
   const cookieVal = $request.headers['Cookie'];
   sy.log(`signurlVal:${signurlVal}`)
   sy.log(`cookieVal:${cookieVal}`)
-  if (signurlVal) sy.setdata(signurlVal, 'sy_signurl_txnews2')
-  if (cookieVal) sy.setdata(cookieVal,  'sy_cookie_txnews2')
+  if (signurlVal) sy.setdata(signurlVal, 'sy_signurl_txnews')
+  if (cookieVal) sy.setdata(cookieVal,  'sy_cookie_txnews')
   sy.msg(cookieName, `获取Cookie: 成功🎉`, ``)
   }
 if ($request && $request.method != 'OPTIONS' && $request.url.match(/user\/event\/report\?/)&&$request.body.indexOf("video_read")!= -1) {
   const videoVal =  $request.url
   sy.log(`videoVal:${videoVal}`)
-  if (videoVal) sy.setdata(videoVal,  'video_txnews2')
+  if (videoVal) sy.setdata(videoVal,  'video_txnews')
   sy.msg(cookieName, `获取视频地址: 成功🎉`, ``)
   }
 
