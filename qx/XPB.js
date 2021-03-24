7e709c8537530bda4767e214b27addcb32d170bf
linwei <61023573+yutushe@users.noreply.github.com>
2021-03-25 01:49:37 +0800

    Update XPB.js

--- qx/XPB.js
+++ qx/XPB.js
@@ -92,7 +92,7 @@ hostname=veishop.iboxpay.com
 
 #笑谱获取更新TOKEN
 https:\/\/veishop\.iboxpay\.com\/nf_gateway\/nf-user-auth-web\/ignore_tk\/veishop\/v1\/* url script-response-body https://raw.githubusercontent.com/ziye66666/JavaScript/main/Task/iboxpay.js
-
+0,30 8-23 * * * https://raw.githubusercontent.com/ziye66666/JavaScript/main/Task/iboxpay.js
 ############## loon
 http-response https:\/\/veishop\.iboxpay\.com\/nf_gateway\/nf-user-auth-web\/ignore_tk\/veishop\/v1\/* script-path=https://raw.githubusercontent.com/ziye66666/JavaScript/main/Task/iboxpay.js, requires-body=1,max-size=0, tag=笑普token
 
